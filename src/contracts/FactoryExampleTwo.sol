f6ba78cd61cc25bbef68eeb9598b1382bf993ef1
Jrigada <jrigada@est.frba.utn.edu.ar>
2024-04-10 16:20:52 -0300

    low level call

--- src/contracts/FactoryExampleTwo.sol
+++ src/contracts/FactoryExampleTwo.sol
@@ -5,6 +5,12 @@ import {TransparentProxyFactory} from 'solidity-utils/src/contracts/transparent-
 
 contract FactoryExampleTwo {
   constructor(address proxyFactory) {
+
+    //low level call that seems to work
+    // (bool success, bytes memory data) = proxyFactory.call(
+    //   abi.encodeWithSignature("createProxyAdmin(address)", address(60000 + 1234))
+    // );
+    
     TransparentProxyFactory(proxyFactory).createProxyAdmin(address(60000 + 1234));
   }
 }
